74842a93a0171225dc7507b7e31000624c423a05
zee-tariq <47204582+zee-tariq@users.noreply.github.com>
2019-01-31 11:47:20 +0500

    Add files via upload

---
+++ src/components/github/StarButton.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const StarButton = () => (
+  <div className="star-button-container">
+    <small>Leave a star on Github if this repository was useful :)</small>
+    <a
+      className="github-button"
+      href="https://github.com/zee-tariq/shoping"
+      data-size="large"
+      aria-label="Star zee-tariq/shoping on GitHub"
+    >
+      Star
+    </a>
+  </div>
+);
+
+export default StarButton;
